a10569ec6c7c692d2ede8fc5c7e8939f3fbedd0f
ihsan-null <ihsanulz92@gmail.com>
2021-08-19 15:10:05 +0700

    Fixing and Add all product from seller and add and edit product from seller

--- routes/routes.go
+++ routes/routes.go
@@ -27,7 +27,7 @@ func New(e *echo.Echo) {
 	e.POST("/seller/login", controller.LoginSeller)
 	e.GET("/seller/:seller_id/products", controller.GetSellerProducts)
 	e.POST("/seller/:seller_id/products", controller.AddProductToSeller)
-	e.PUT("/seller/seller_id/products/product_id", controller.EditSellerProduct)
+	e.PUT("/seller/:seller_id/products/:product_id", controller.EditSellerProduct)
 
 	//GET list data of all markets available
 	e.GET("/markets", controller.GetAllMarket)
