8464e38ffbdce07fcecb6ab2d3a7d749896c3f99
FawwazAF <fawwazaf123@gmail.com>
2021-08-21 15:06:02 +0700

    add unit testing

--- config/config.go
+++ config/config.go
@@ -43,3 +43,13 @@ func InitMigrate() {
 	DB.AutoMigrate(&models.Order{})
 	DB.AutoMigrate(&models.Payment{})
 }
+
+func ConfigTest() (*gorm.DB, error) {
+	var err error
+	connectionStringTest := "root:Minus12345@tcp(localhost:3306)/etmarket_test?charset=utf8&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(connectionStringTest), &gorm.Config{})
+	if err != nil {
+		return DB, err
+	}
+	return DB, err
+}
