79b6f742be7adb1e3136234104bc1061c424f370
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-08-25 13:36:19 +0700

    delete unimportant things

--- main.go
+++ main.go
@@ -4,10 +4,8 @@ import (
 	"etmarket/project/config"
 	"etmarket/project/routes"
 	"fmt"
-	"net/http"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -16,10 +14,6 @@ func main() {
 	config.InitDb()
 	config.InitPort()
 	routes.New(e)
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
-	}))
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.HTTP_PORT)))
 
 }
