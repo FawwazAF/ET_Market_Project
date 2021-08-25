fae4c86b1157101d21a8795ba7c588d0ea461e72
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-08-25 13:30:20 +0700

    fixing output get all categories

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"etmarket/project/config"
 	"etmarket/project/routes"
 	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -14,6 +16,10 @@ func main() {
 	config.InitDb()
 	config.InitPort()
 	routes.New(e)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+	}))
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.HTTP_PORT)))
 
 }
