706c1385d6d3e72d1bce2c9e93a629960f50a9cb
Riska Kurnia Dewi <riskakurniad@gmail.com>
2021-08-25 12:23:12 +0700

    finishing routes

--- controller/checkoutGetController.go
+++ controller/checkoutGetController.go
@@ -9,10 +9,10 @@ import (
 )
 
 // Ihsan
-func GetCheckoutStatusInProgress(c echo.Context) error {
+func GetCheckoutStatus(c echo.Context) error {
 	logged_in_user_id := middlewares.ExtractToken(c)
 	status := c.QueryParam("status")
-	inprogress_checkout, err := database.GetHistoryInProgress(status, logged_in_user_id)
+	inprogress_checkout, err := database.GetHistory(status, logged_in_user_id)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
 			"message": "request not found",
@@ -21,39 +21,5 @@ func GetCheckoutStatusInProgress(c echo.Context) error {
 	return c.JSON(http.StatusOK, inprogress_checkout)
 }
 func GetCheckoutStatusTesting() echo.HandlerFunc {
-	return GetCheckoutStatusInProgress
+	return GetCheckoutStatus
 }
-
-// // Ihsan
-// func GetCheckoutStatusComplete(c echo.Context) error {
-// 	logged_in_user_id := middlewares.ExtractToken(c)
-// 	status := c.QueryParam("status")
-// 	complete_checkout, err := database.GetHistoryInProgress(status, logged_in_user_id)
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-// 			"message": "request not found",
-// 		})
-// 	}
-// 	return c.JSON(http.StatusOK, map[string]interface{}{
-// 		"message": "request success",
-// 		"data":    complete_checkout,
-// 	})
-// }
-
-// Ihsan
-// func GetSelectedOrder(c echo.Context) error {
-// 	checkout_id, err := strconv.Atoi(c.Param("checkout_id"))
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{
-// 			"message": "invalid checkout id",
-// 		})
-// 	}
-// 	data, err := database.GetSelectedOrder(checkout_id)
-// 	if err != nil {
-// 		return c.JSON(http.StatusBadRequest, err.Error())
-// 	}
-// 	return c.JSON(http.StatusOK, map[string]interface{}{
-// 		"message": "succesfully retrieve data",
-// 		"data":    data,
-// 	})
-// }
